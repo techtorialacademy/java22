db890708f0c91557d2452ff042e79cc75e5964e4
techtorialacademy <yusufcanturan@icloud.com>
2024-12-14 10:27:12 -0600

    Update Git Conflict by conflict branch

--- src/test/java/FeatureTesting/GitConflicPractice.java
+++ src/test/java/FeatureTesting/GitConflicPractice.java
@@ -3,6 +3,8 @@ package FeatureTesting;
 public class GitConflicPractice {
 
     public static void main(String[] args) {
+        System.out.println("This change comes from conflict branch");
+        System.out.println("This change comes from conflict branch and should not be deleted.");
 
 
 
