685a45d120a2effd79fcd6166eb984014fa144cc
techtorialacademy <yusufcanturan@icloud.com>
2024-12-14 10:25:52 -0600

    Update GitConflict

--- src/test/java/FeatureTesting/GitConflicPractice.java
+++ src/test/java/FeatureTesting/GitConflicPractice.java
@@ -3,7 +3,8 @@ package FeatureTesting;
 public class GitConflicPractice {
 
     public static void main(String[] args) {
-
+        System.out.println("This change comes from git management branch.");
+        System.out.println("This change comes from git management branch and should not be deleted.");
 
 
 
